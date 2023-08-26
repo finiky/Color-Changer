b2a05b457a7b0358047f106dc16511c3d5ef6d90
Akshay <98738875+finiky@users.noreply.github.com>
2023-08-26 20:04:19 +0530

    wrote a color chaning program using css modules, useState and useEffect

---
+++ app/src/App.js
@@ -0,0 +1,51 @@
+import styles from "./App.module.css";
+import { useEffect, useState } from "react";
+const App = () => {
+  const [color, setColor] = useState("white");
+  useEffect(() => {
+    document.getElementById("main").style.backgroundColor = color;
+    document.getElementsByTagName("body")[0].style.backgroundColor = color;
+    document.getElementById("div1").style.backgroundColor = color;
+  }, [color]);
+  return (
+    <div id="main" className={styles.main}>
+      <header className={styles.header}>
+        <h1>Color Changer</h1>
+        <h2>Change background color of the page with a click!</h2>
+      </header>
+      <div id="div1" className={styles.section}>
+        <aside className={styles.aside}>
+          <h3 className={styles.h3}>Click on a color</h3>
+          <button
+            onClick={() => setColor("red")}
+            id="red"
+            className={styles.red}
+          ></button>
+          <button
+            onClick={() => setColor("blue")}
+            id="blue"
+            className={styles.blue}
+          ></button>
+          <button
+            onClick={() => setColor("green")}
+            id="green"
+            className={styles.green}
+          ></button>
+          <button
+            onClick={() => setColor("pink")}
+            id="pink"
+            className={styles.pink}
+          ></button>
+          <button
+            onClick={() => setColor("yellow")}
+            id="yellow"
+            className={styles.yellow}
+          ></button>
+        </aside>
+        <div className={styles.div}></div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
